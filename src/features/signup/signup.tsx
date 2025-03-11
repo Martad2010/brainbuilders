84995ecbff8f4a99df99fc61bfb1bf7763a89cf0
ifeoluwabk4ever <ifeoluwabk4ever@gmail.com>
2025-03-11 17:04:17 +0100

    Suspense Wrapper for Signup

--- src/features/signup/signup.tsx
+++ src/features/signup/signup.tsx
@@ -1,19 +1,23 @@
+"use client";
+import { Suspense } from "react";
 import SignUpForm from "@/components/signup/SignupForm";
 
 export const SignUp = () => {
   return (
-    <main
-      className="flex min-h-screen px-0 md:px-12"
-      style={{
-        background: 'url("/images/overview-hero-bg.svg")',
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-        backgroundRepeat: "no-repeat",
-      }}
-    >
-      <div className="container flex flex-col items-center justify-center lg:items-start">
-        <SignUpForm />
-      </div>
-    </main>
+    <Suspense fallback={<></>}>
+      <main
+        className="flex min-h-screen px-0 md:px-12"
+        style={{
+          background: 'url("/images/overview-hero-bg.svg")',
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+        }}
+      >
+        <div className="container flex flex-col items-center justify-center lg:items-start">
+          <SignUpForm />
+        </div>
+      </main>
+    </Suspense>
   );
 };
